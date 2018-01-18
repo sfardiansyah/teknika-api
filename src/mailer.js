944de7059a2a66ccf188e0369297e16279f79228
sfardiansyah <syahrul.findi@gmail.com>
2018-01-18 20:06:29 +0700

    Add email confirmation.

---
+++ src/mailer.js
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+const from = '"Teknika FTUI" <info@teknikaftui.net>';
+
+function setup() {
+    return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+}
+
+export function sendConfirmationEmail(user) {
+    const transport = setup();
+    const email = {
+        from,
+        to: user.email,
+        subject: "Selamat Bergabung di Teknika FTUI",
+        text: `
+        Selamat Bergabung di Teknika FTUI. Mohon konfirmasi email anda.
+
+        ${user.generateConfirmationUrl()}
+        `
+    };
+
+    transport.sendMail(email);
+}
